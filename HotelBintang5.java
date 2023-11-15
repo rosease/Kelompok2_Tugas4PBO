cffba75e9b9e1da1979aab80226484c4b03e7291
Sa' Baniatun Astia <121396151+rosease@users.noreply.github.com>
2023-11-15 09:22:25 +0700

    HotelBintang5.java

--- HotelBintang5.java
+++ HotelBintang5.java
@@ -1,6 +1,6 @@
 // Kelas turunan fasilitas untuk Hotel Bintang 5
 class HotelBintang5 extends Fasilitas {
     public HotelBintang5() {
-        super("Hotel Bintang 5", 500.0); // Biaya tambahan untuk Hotel Bintang 5
+        super("Hotel Bintang 5", 500000); // Biaya tambahan untuk Hotel Bintang 5
     }
 }
