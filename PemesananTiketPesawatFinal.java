7516921c2a18d330e4f004c3150fdf41d3c04351
fadiana1 <149872827+fadiana1@users.noreply.github.com>
2023-11-15 08:16:04 +0700

    Update PemesananTiketPesawatFinal.java

--- PemesananTiketPesawatFinal.java
+++ PemesananTiketPesawatFinal.java
@@ -54,6 +54,21 @@ public class PemesananTiketPesawat {
         return (getHarga() * jumlahTiket * 2) + fasilitas.getBiaya();
     }
 }
+    class TiketPesawatNonPP extends TiketPesawat {
+    private int jumlahTiket;
+    private Fasilitas fasilitas;
+
+    public TiketPesawatNonPP(String maskapai, String tujuan, double harga, int jumlahTiket, Fasilitas fasilitas) {
+        super(maskapai, tujuan, harga);
+        this.jumlahTiket = jumlahTiket;
+        this.fasilitas = fasilitas;
+    }
+
+    @Override
+    public double hitungTotalHarga() {
+        return (getHarga() * jumlahTiket) + fasilitas.getBiaya();
+    }
+}
     
     
     public static void main(String[] args) {
