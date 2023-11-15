f737f3c743bc4760913b64252b8d6aae94b127b2
Sa' Baniatun Astia <121396151+rosease@users.noreply.github.com>
2023-11-15 09:23:09 +0700

    HotelBintang3.java

--- HotelBintang3.java
+++ HotelBintang3.java
@@ -1,5 +1,5 @@
 class HotelBintang3 extends Fasilitas {
     public HotelBintang3() {
-        super("Hotel Bintang 3", 350.0); // Biaya tambahan untuk Hotel Bintang 3
+        super("Hotel Bintang 3", 350000); // Biaya tambahan untuk Hotel Bintang 3
     }
 }
