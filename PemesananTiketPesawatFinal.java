97c307de1b65d398f6cc84e30be63584bc42ffcd
fadiana1 <149872827+fadiana1@users.noreply.github.com>
2023-11-15 08:16:47 +0700

    Update PemesananTiketPesawatFinal.java

--- PemesananTiketPesawatFinal.java
+++ PemesananTiketPesawatFinal.java
@@ -69,6 +69,32 @@ public class PemesananTiketPesawat {
         return (getHarga() * jumlahTiket) + fasilitas.getBiaya();
     }
 }
+    //Membuat kelas fasilitas
+class Fasilitas {
+    private String jenis;
+    private double biaya;
+
+    public Fasilitas(String jenis, double biaya) {
+        this.jenis = jenis;
+        this.biaya = biaya;
+    }
+    
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public double getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(double biaya) {
+        this.biaya = biaya;
+    }
+}
     
     
     public static void main(String[] args) {
